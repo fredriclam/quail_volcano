Modify bug in Quail DG.py for empty resL, resR having seemingly wrong shape. Add 2D, 2D2D capability. Add logging to multidomain.

src/physics/multiphasevpT/functions.py
@@ -43,6 +43,8 @@ class FcnType(Enum):
 	RiemannProblem = auto()
 	GravityRiemann = auto()
 	UniformExsolutionTest = auto()
+	IsothermalAtmosphere = auto()
+	LinearAtmosphere = auto()
 
 
 class BCType(Enum):
@@ -234,6 +236,123 @@ class UniformExsolutionTest(FcnBase):
 		Uq[:, :, iarhoWv] = arhoWv
 		Uq[:, :, iarhoM] = arhoM
 		Uq[:, :, imom] = rho * u
+		Uq[:, :, ie] = e
+		# Tracer quantities
+		Uq[:, :, iarhoWt] = arhoWt
+		Uq[:, :, iarhoC] = arhoC
+
+		return Uq # [ne, nq, ns]
+
+
+class IsothermalAtmosphere(FcnBase):
+	'''
+	Isothermal air atmosphere as an initial condition.
+	'''
+
+	def __init__(self,T:float=300., p_atm:float=1e5,
+		h0:float=0.0, gravity:float=9.8):
+		''' Set atmosphere temperature, pressure, and location of pressure.
+		Pressure distribution is computed as hydrostatic profile with p = p_atm
+		at elevation h0.
+		'''
+		self.T = T
+		self.p_atm = p_atm
+		self.h0 = h0
+		self.gravity = gravity
+
+	def get_state(self, physics, x, t):
+		# Unpack
+		Uq = np.zeros([x.shape[0], x.shape[1], physics.NUM_STATE_VARS])
+		iarhoA, iarhoWv, iarhoM, irhou, irhov, ie, iarhoWt, iarhoC = \
+			physics.get_state_indices()
+
+		# Compute scale height
+		hs = physics.Gas[0]["R"]*self.T/self.gravity
+		# Compute pressure
+		p = self.p_atm * np.exp(-(x[:,:,1:2] - self.h0)/hs).squeeze(axis=2)
+		# Pure air density
+		arhoA = p / (physics.Gas[0]["R"]*self.T)
+		# Zero or trace amounts of Wv, M and tracers
+		arhoWv = np.zeros_like(p)
+		arhoM = np.zeros_like(p)
+		arhoWt = np.zeros_like(p)
+		arhoC = np.zeros_like(p)
+		# Zero velocity
+		u = np.zeros_like(p)
+		v = np.zeros_like(p)
+
+		rho = arhoA + arhoWv + arhoM
+
+		e = (arhoA * physics.Gas[0]["c_v"] * self.T + 
+			arhoWv * physics.Gas[1]["c_v"] * self.T + 
+			arhoM * (physics.Liquid["c_m"] * self.T + physics.Liquid["E_m0"])
+			+ 0.5 * rho * u**2.)
+		
+		Uq[:, :, iarhoA] = arhoA
+		Uq[:, :, iarhoWv] = arhoWv
+		Uq[:, :, iarhoM] = arhoM
+		Uq[:, :, irhou] = rho * u
+		Uq[:, :, irhov] = rho * v
+		Uq[:, :, ie] = e
+		# Tracer quantities
+		Uq[:, :, iarhoWt] = arhoWt
+		Uq[:, :, iarhoC] = arhoC
+
+		return Uq # [ne, nq, ns]
+
+class LinearAtmosphere(FcnBase):
+	'''
+	Constant-density atmosphere (isopycnic) as an initial condition. The
+	Temperature is adjusted to keep the density constant (and thus the pressure
+	a linear function of elevation).
+	'''
+
+	def __init__(self,T0:float=300., p_atm:float=1e5,
+		h0:float=0.0, gravity:float=9.8):
+		''' Set atmosphere temperature, pressure, and location of pressure.
+		Pressure distribution is computed as hydrostatic profile with p = p_atm
+		at elevation h0.
+		'''
+		self.T0 = T0
+		self.p_atm = p_atm
+		self.h0 = h0
+		self.gravity = gravity
+
+	def get_state(self, physics, x, t):
+		# Unpack
+		Uq = np.zeros([x.shape[0], x.shape[1], physics.NUM_STATE_VARS])
+		iarhoA, iarhoWv, iarhoM, irhou, irhov, ie, iarhoWt, iarhoC = \
+			physics.get_state_indices()
+
+		# Compute scale height at reference temperature T0
+		hs0 = physics.Gas[0]["R"]*self.T0/self.gravity
+		# Compute pressure linear in elevation
+		p = self.p_atm * (1.0 - (x[:,:,1:2] - self.h0)/hs0).squeeze(axis=2)
+		# Constant pure air density at h0
+		arhoA = self.p_atm / (physics.Gas[0]["R"]*self.T0)
+		# Compute temperature
+		T = p / (arhoA * physics.Gas[0]["R"])
+		# Zero or trace amounts of Wv, M and tracers
+		arhoWv = np.zeros_like(p)
+		arhoM = np.zeros_like(p)
+		arhoWt = np.zeros_like(p)
+		arhoC = np.zeros_like(p)
+		# Zero velocity
+		u = np.zeros_like(p)
+		v = np.zeros_like(p)
+
+		rho = arhoA + arhoWv + arhoM
+
+		e = (arhoA * physics.Gas[0]["c_v"] * T + 
+			arhoWv * physics.Gas[1]["c_v"] * T + 
+			arhoM * (physics.Liquid["c_m"] * T + physics.Liquid["E_m0"])
+			+ 0.5 * rho * u**2.)
+		
+		Uq[:, :, iarhoA] = arhoA
+		Uq[:, :, iarhoWv] = arhoWv
+		Uq[:, :, iarhoM] = arhoM
+		Uq[:, :, irhou] = rho * u
+		Uq[:, :, irhov] = rho * v
 		Uq[:, :, ie] = e
 		# Tracer quantities
 		Uq[:, :, iarhoWt] = arhoWt
@@ -1024,7 +1143,7 @@ class Euler2D2D(BCWeakRiemann):
 class MultiphasevpT1D1D(BCWeakRiemann):
 	'''
 	This class implements a 1D-1D coupling boundary condition.
-	The interior convective flux and the artifical viscosity diffusion integral
+	TODO: The numerical flux for the artifical viscosity diffusion integral
 	need to be dealt with.
 
 	Attributes:
@@ -1056,6 +1175,41 @@ class MultiphasevpT1D1D(BCWeakRiemann):
 			copy.copy(physics.bdry_data_net[data_net_key]["bdry_face_state"]),
 			axis=(0,1))
 
+
+class MultiphasevpT2D2D(BCWeakRiemann):
+	'''
+	This class implements a 2D-2D coupling boundary condition.
+	TODO: The numerical flux for the artifical viscosity diffusion integral
+	need to be dealt with.
+
+	Attributes:
+	-----------
+	bkey (str): Key corresponding to boundary as edge in domain graph; typically
+	            the name of the boundary.
+	'''
+
+	def __init__(self, bkey):
+		self.bkey = bkey
+
+	def get_extrapolated_state(self, physics, UqI, normals, x, t):
+		''' Called when computing states at shared boundaries. '''
+		return UqI
+
+	def get_boundary_state(self, physics, UqI, normals, x, t):
+		''' Get shared state UqI and return (BCWeakRiemann) '''
+		# Get id string of adjacent domain from domain graph (local)
+		adjacent_domain_id = [
+			key for key in 
+			physics.domain_edges[physics.domain_id][self.bkey] 
+			if key != physics.domain_id][0]
+		# Get key for boundary state in shared memory (via Manager.dict)
+		data_net_key = physics.edge_to_key(
+										 physics.domain_edges[physics.domain_id][self.bkey],
+										 adjacent_domain_id)
+		# Return orientation-corrected exterior state
+		return np.flip(
+			copy.copy(physics.bdry_data_net[data_net_key]["bdry_face_state"]),
+			axis=(0,1))
 
 '''
 ---------------------
@@ -1392,26 +1546,24 @@ class LaxFriedrichs2D(ConvNumFluxBase):
 		n_hat = normals/n_mag
 
 		# Left flux
-		FqL, (u2L, v2L, rhoL, pL) = physics.get_conv_flux_projected(UqL,
+		FqL, (u2L, v2L, aL) = physics.get_conv_flux_projected(UqL,
 				n_hat)
 
 		# Right flux
-		FqR, (u2R, v2R, rhoR, pR) = physics.get_conv_flux_projected(UqR,
+		FqR, (u2R, v2R, aR) = physics.get_conv_flux_projected(UqR,
 				n_hat)
 
 		# Jump
 		dUq = UqR - UqL
 
 		# Max wave speeds at each point
-		aL = np.empty(pL.shape + (1,))
-		aR = np.empty(pR.shape + (1,))
-		aL[:, :, 0] = np.sqrt(u2L + v2L) + np.sqrt(physics.gamma * pL / rhoL)
-		aR[:, :, 0] = np.sqrt(u2R + v2R) + np.sqrt(physics.gamma * pR / rhoR)
-		idx = aR > aL
-		aL[idx] = aR[idx]
+		wL = np.empty(u2L.shape + (1,))
+		wR = np.empty(u2R.shape + (1,))
+		wL[:, :, 0] = np.sqrt(u2L + v2L) + aL
+		wR[:, :, 0] = np.sqrt(u2R + v2R) + aR
 
 		# Put together
-		return 0.5 * n_mag * (FqL + FqR - aL*dUq)
+		return 0.5 * n_mag * (FqL + FqR - np.maximum(wL, wR)*dUq)
 
 
 class Roe1D(ConvNumFluxBase):

src/physics/multiphasevpT/multiphasevpT.py
@@ -25,6 +25,7 @@
 # ------------------------------------------------------------------------ #
 from enum import Enum
 import numpy as np
+from pkg_resources import NullProvider
 
 import errors
 import general
@@ -73,7 +74,7 @@ class MultiphasevpT(base.PhysicsBase):
 			BCType.SlipWall : mpvpT_fcns.SlipWall,
 			# BCType.PressureOutlet : mpvpT_fcns.PressureOutlet,
 			# BCType.MultiphasevpT2D1D: mpvpT_fcns.MultiphasevpT2D1D,
-			# BCType.MultiphasevpT2D2D: mpvpT_fcns.MultiphasevpT2D2D,
+			BCType.MultiphasevpT2D2D: mpvpT_fcns.MultiphasevpT2D2D,
 			BCType.MultiphasevpT1D1D: mpvpT_fcns.MultiphasevpT1D1D,
 		})
 
@@ -473,7 +474,7 @@ class MultiphasevpT1D(MultiphasevpT):
 		mass_indices = [self.get_state_index("pDensityA"),
 										self.get_state_index("pDensityWv"),
 										self.get_state_index("pDensityM")]
-		ifirst = np.min(mass_indices)
+		# TODO: implement for non-contiguous states
 		return slice(np.min(mass_indices), np.min(mass_indices)+len(mass_indices))
 
 	def get_momentum_slice(self):
@@ -495,12 +496,9 @@ class MultiphasevpT1D(MultiphasevpT):
 
 		# Compute mixture (total) density
 		rho = arhoA + arhoWv + arhoM
-		p = self.compute_additional_variable("Pressure", Uq, True)
-		p = np.squeeze(p, axis=2)
-		# Compute velocity vector
+		p = self.compute_additional_variable("Pressure", Uq, True).squeeze(axis=2)
 		u = mom / rho
-		# Compute squared norm of velocity
-		u2 = u**2.
+		u2 = u**2.0
 		# u2 = np.sum(mom**2, axis=2, keepdims=True) / np.power(rho, 2.)
 		
 		# Construct physical flux
@@ -629,7 +627,8 @@ class MultiphasevpT2D(MultiphasevpT):
 		super().set_maps()
 
 		d = {
-			FcnType.Ambient: lambda:(blah for blah in ()).throw(NotImplementedError("Nothing in Ambient.")),
+			FcnType.IsothermalAtmosphere: mpvpT_fcns.IsothermalAtmosphere,
+			FcnType.LinearAtmosphere: mpvpT_fcns.LinearAtmosphere,
 			# euler_fcn_type.IsentropicVortex : euler_fcns.IsentropicVortex,
 			# euler_fcn_type.TaylorGreenVortex : euler_fcns.TaylorGreenVortex,
 			# euler_fcn_type.GravityRiemann : euler_fcns.GravityRiemann,
@@ -640,12 +639,12 @@ class MultiphasevpT2D(MultiphasevpT):
 		self.BC_fcn_map.update(d)
 
 		self.source_map.update({
-			SourceType.GravitySource: lambda:(blah for blah in ()).throw(NotImplementedError("Nothing in Ambient.")),
+			SourceType.GravitySource: mpvpT_fcns.GravitySource,
 		})
 
 		self.conv_num_flux_map.update({
 			base_conv_num_flux_type.LaxFriedrichs:
-				mpvpT_fcns.mpvpT_fcns.LaxFriedrichs1D,
+				mpvpT_fcns.LaxFriedrichs2D,
 		})
 
 	class StateVariables(Enum):
@@ -669,6 +668,13 @@ class MultiphasevpT2D(MultiphasevpT):
 		iarhoC = self.get_state_index("pDensityC")
 		return iarhoA, iarhoWv, iarhoM, irhou, irhov, ie, iarhoWt, iarhoC
 
+	def get_mass_slice(self):
+		# Get mass component indices
+		mass_indices = [self.get_state_index("pDensityA"),
+										self.get_state_index("pDensityWv"),
+										self.get_state_index("pDensityM")]
+		return slice(np.min(mass_indices), np.min(mass_indices)+len(mass_indices))
+
 	def get_momentum_slice(self):
 		irhou = self.get_state_index("XMomentum")
 		irhov = self.get_state_index("YMomentum")
@@ -691,31 +697,31 @@ class MultiphasevpT2D(MultiphasevpT):
 		# Extract momentum in vector form ([n, nq, ndims])
 		mom    = Uq[:, :, smom]
 
-		# Compute intermediate quantities
-		u = rhou / rho
-		v = rhov / rho
-		u2 = u**2
-		v2 = v**2
-		vel = mom / rho
-		rhouv = rhou * v
 		# Compute mixture (total) density
 		rho = arhoA + arhoWv + arhoM
 		p = self.compute_additional_variable("Pressure", Uq, True).squeeze(axis=2)
+		u = rhou / rho
+		v = rhov / rho
+		u2 = u**2.0
+		v2 = v**2.0
+		rhouv = rhou * v
+		# Vector
+		vel = mom / np.expand_dims(rho,axis=2)
 
 		# Construct physical flux
 		F = np.empty(Uq.shape + (self.NDIMS,)) # [n, nq, ns, ndims]
-		F[:, :, iarhoA,  :] = arhoA * vel         # Flux of massA in all directions
-		F[:, :, iarhoWv, :] = arhoWv * vel        # Flux of massWv in all directions
-		F[:, :, iarhoM,  :] = arhoM * vel         # Flux of massM in all directions
+		# Compute flux of non-tracer mass quantities in all directions
+		F[:, :, self.get_mass_slice(),  :] = np.einsum(
+			"ijk, ijl -> ijkl", Uq[:,:,self.get_mass_slice()], vel)
 		F[:, :, irhou,   0] = rho * u2 + p        # x-flux of x-momentum
 		F[:, :, irhov,   0] = rhouv               # x-flux of y-momentum
 		F[:, :, irhou,   1] = rhouv               # y-flux of x-momentum
 		F[:, :, irhov,   1] = rho * v2 + p        # y-flux of y-momentum
-		F[:, :, ie,      :] = (e + p) * vel       # Flux of energy in all directions
-		F[:, :, iarhoWt, :] = arhoWt * vel        # Flux of massWt in all directions
-		F[:, :, iarhoC,  :] = arhoC * vel         # Flux of massC in all directions
+		F[:, :, ie,      :] = np.expand_dims(e + p,axis=2)    # Flux of energy in all directions
+		F[:, :, iarhoWt, :] = np.expand_dims(arhoWt,axis=2)   # Flux of massWt in all directions
+		F[:, :, iarhoC,  :] = np.expand_dims(arhoC,axis=2)    # Flux of massC in all directions
 
 		# Compute sound speed
 		a = self.compute_additional_variable("SoundSpeed", Uq, True).squeeze(axis=2)
 
-		return F, (u2, v2, rho, p, a)
+		return  F, (u2, v2, a)